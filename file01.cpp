f12410c984b76d5b783df8da8c47a6bffe05455b
Nishchay Gupta <ngnishchay191@gmail.com>
2019-10-22 23:26:07 +0530

    d Initial commit

---
+++ file01.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string greet(string name) {
+    cout<<"hello "<<name;
+}
+int main(int argc, char** argv) {
+    greet("Nishchay");
+}
---
+++ file02.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout<<"learning git"<<endl;
+    cout<<"use git status to check status"<<endl;
+    cout<<"use git log to see history"<<endl;
+}
